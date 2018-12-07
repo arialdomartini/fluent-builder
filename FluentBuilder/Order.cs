890b64e5dfe70f927b3a6811d76b51e042904e1f
Arialdo Martini <arialdomartini@gmail.com>
2018-12-07 22:22:44 +0100

    Builder assigns a simple value

---
+++ FluentBuilder/Order.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Xunit;
+using static FluentBuilder.OrderBuilder;
+namespace FluentBuilder
+{
+    public class OrderBuilder
+    {
+        private int _number;
+
+        public static OrderBuilder Order() => new OrderBuilder();
+
+        public OrderBuilder WithNumber(int i)
+        {
+            _number = i;
+            return this;
+        }
+
+
+        public Order Build() =>
+            new Order
+            {
+                Number = _number
+            };
+    }
+
+    public class Order
+    {
+        public int Number { get; set; }
+    }
+
+    
+    public class BuilderClient
+    {
+        [Fact]
+        public void should_build_an_order()
+        {
+            var result = Order()
+                .WithNumber(10)
+                .Build();
+
+            result.Number.Should().Be(10);
+        }
+    }
+}
