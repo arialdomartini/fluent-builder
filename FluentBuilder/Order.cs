90f3867ec943945097d0016efa5e426938ca23a9
Arialdo Martini <arialdomartini@gmail.com>
2018-12-09 09:36:56 +0100

    A more complete example

--- FluentBuilder/Order.cs
+++ FluentBuilder/Order.cs
@@ -10,11 +10,20 @@ namespace FluentBuilder
         private int _number;
         private DateTime _date;
 
-        private readonly ArticleBuilder _articleBuilder;
-        private readonly CustomerBuilder _customerBuilder;
+        private ArticleBuilder _articleBuilder;
+        private CustomerBuilder _customerBuilder;
 
-        public static OrderBuilder AnOrder() =>
+        public static OrderBuilder AnEmptyOrder() =>
             new OrderBuilder();
+
+        public static OrderBuilder AnOrderWithOneArticle() => 
+            AnEmptyOrder()
+                .WithNumber(100)
+                .CreatedOn(new DateTime(2018, 10, 10))
+                .ContainingAnArticle(a => a
+                    .InCategory("some category")
+                    .WithPrice(50.50m));
+
 
         private OrderBuilder()
         {
@@ -28,19 +37,19 @@ namespace FluentBuilder
             return this;
         }
 
-        public OrderBuilder WithDate(DateTime dateTime)
+        public OrderBuilder CreatedOn(DateTime dateTime)
         {
             _date = dateTime;
             return this;
         }
 
-        public OrderBuilder HavingAnArticle(Func<ArticleBuilder, ArticleBuilder> func)
+        public OrderBuilder ContainingAnArticle(Func<ArticleBuilder, ArticleBuilder> func)
         {
             func(_articleBuilder);
             return this;
         }
         
-        public OrderBuilder HavingCustomer(Func<CustomerBuilder, CustomerBuilder> func)
+        public OrderBuilder ByACustomer(Func<CustomerBuilder, CustomerBuilder> func)
         {
             func(_customerBuilder);
             return this;
@@ -59,8 +68,9 @@ namespace FluentBuilder
     public class CustomerBuilder
     {
         private string _name;
+        private DateTime _birthday;
 
-        public CustomerBuilder WithName(string name)
+        public CustomerBuilder Named(string name)
         {
             _name = name;
             return this;
@@ -69,8 +79,21 @@ namespace FluentBuilder
         public Customer Build() => 
             new Customer
             {
-                Name = _name
+                Name = _name,
+                BirthDay = _birthday
             };
+
+        public CustomerBuilder BornOn(DateTime dateTime)
+        {
+            _birthday = dateTime;
+            return this;
+        }
+
+        public CustomerBuilder Aged(int age)
+        {
+            _birthday = DateTime.Now.AddYears(-age);
+            return this;
+        }
     }
 
     public class ArticleBuilder
@@ -93,7 +116,7 @@ namespace FluentBuilder
             };
         }
 
-        public ArticleBuilder WithCategory(string category)
+        public ArticleBuilder InCategory(string category)
         {
             _category = category;
             return this;
@@ -111,6 +134,7 @@ namespace FluentBuilder
     public class Customer
     {
         public string Name { get; set; }
+        public DateTime BirthDay { get; set; }
     }
 
     public class Article
@@ -124,14 +148,15 @@ namespace FluentBuilder
         [Fact]
         public void should_build_an_order()
         {
-            var order = AnOrder()
+            var order = AnEmptyOrder()
                 .WithNumber(10)
-                .WithDate(new DateTime(2018, 12, 24))
-                .HavingAnArticle(a => a
+                .CreatedOn(new DateTime(2018, 12, 24))
+                .ByACustomer(c => c
+                    .Named("Amelia")
+                    .Aged(42))
+                .ContainingAnArticle(a => a
                     .WithPrice(32.50m)
-                    .WithCategory("books"))
-                .HavingCustomer(c => c
-                    .WithName("Amelia"))
+                    .InCategory("books"))
                 .Build();
 
             order.Number.Should().Be(10);
